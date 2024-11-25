83d0b21c3b4d9d5af4c1da1a733a523d72a01f9c
unknown <matiasalemany2@gmail.com>
2024-11-25 18:29:55 +0000

    Creating REST API

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+    constructor(private readonly coffeesService: CoffeesService) {
+    }
+
+    //Static routes
+    @Get()
+    findAll(@Query() paginationQuery) {
+        const {limit, offset} = paginationQuery;
+        return this.coffeesService.findAll();
+    }
+
+    //Dinamic routes
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.coffeesService.findOne(id);
+    }
+
+    //POST
+    @Post()
+    @HttpCode(HttpStatus.GONE)
+    create(@Body() createCoffeeDto: CreateCoffeeDto) {
+        return this.coffeesService.create(createCoffeeDto);
+    }
+
+    //PATCH - Update    
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+        return this.coffeesService.update(id, updateCoffeeDto);
+    }
+
+    //DELETE - Remove
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.coffeesService.remove(id)}
+        
+}
