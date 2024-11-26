627439ea18819e63e07c910b508efba4b60289f1
unknown <matiasalemany2@gmail.com>
2024-11-26 18:32:19 +0000

    Started adding PostgreSQL with TypeORM

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity() // sql table === "coffee"
 export class Coffee {
+    @PrimaryGeneratedColumn() // sql column === "id"
     id: number;
+
+    @Column() // sql column === "name"
     name: string;
+
+    @Column() // sql column === "price"
     brand: string;
+
+    @Column('json', {nullable: true}) // sql column === "flavors"
     flavors: string[];
 }
