e25edd156541e00189f19233f84bd525dcd1d463
unknown <matiasalemany2@gmail.com>
2024-12-12 16:18:23 +0100

    mongodb added

---
+++ src/events/entities/event-entity/event-entity.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import * as mongoose from 'mongoose'
+
+@Schema()
+export class Event extends mongoose.Document {
+    @Prop()
+    type: string;
+
+    @Prop({index: true})
+    name: string;
+
+    @Prop({type: Object})
+    payload: Record<string, any>
+}
+
+export const EventSchema = SchemaFactory.createForClass(Event)
+
+
+
